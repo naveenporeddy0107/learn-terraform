2070deca30bf4e34e70a6bfd57a94b00374b91a0
nreddy4 <naveenkumar9707@gmail.com>
2023-05-30 13:55:06 -0500

    first terraforma example for creating ec2 instance

--- variables/main.tf
+++ variables/main.tf
@@ -10,24 +10,11 @@ variable "integer" {
   default=10.5
 }
 
-output "boolean"{
-  value=var.boolean
-}
-output "float"{
-  value=var.integer
-}
-
-output "resultofsingle"{
-  value=var.single
-}
 
 variable "list"{
   default =["naveen","rakshitha",10,"kruthika"]
 }
 
-output "output_list" {
-  value="${var.list[1]} >>>> ${var.list[2]} >>>> ${var.list[3]}"
-}
 
 variable "dictionary" {
   default ={
@@ -36,6 +23,3 @@ variable "dictionary" {
   }
 }
 
-output "dictvalue"{
-  value=var.dictionary["age"]
-}
---
+++ variables/output.tf
@@ -0,0 +1,16 @@
+output "boolean"{
+  value=var.boolean
+}
+output "float"{
+  value=var.integer
+}
+
+output "resultofsingle"{
+  value=var.single
+}
+output "output_list" {
+  value="${var.list[1]} >>>> ${var.list[2]} >>>> ${var.list[3]}"
+}
+output "dictvalue"{
+  value="${var.dictionary["age"]}"
+}
