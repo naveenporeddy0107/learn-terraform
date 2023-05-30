95a7e198807e2788f1d175fd38bc41ee2d212952
nreddy4 <naveenkumar9707@gmail.com>
2023-05-30 13:47:55 -0500

    first terraforma example for creating ec2 instance

---
+++ variables/main.tf
@@ -0,0 +1,15 @@
+variable "single" {
+  default="learning terraform"
+}
+
+output "resultofsingle"{
+  value=var.single
+}
+
+variable "list"{
+  default =["naveen","rakshitha",10,"kruthika"]
+}
+
+output "output_list" {
+  value="${var.list[1]} >>>> ${var.list[2]}"
+}
