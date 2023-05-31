4702e8d5cefe3db850aea3454e20e09eda4c74b4
nreddy4 <naveenkumar9707@gmail.com>
2023-05-30 20:44:25 -0500

    first terraforma example for creating ec2 instance

--- variables/main.tf
+++ variables/main.tf
@@ -1,25 +1,25 @@
 variable "single" {
-  default="learning terraform"
+  default = "learning terraform"
 }
 
 variable "boolean" {
-  default=true
+  default = true
 }
 
 variable "integer" {
-  default=10.5
+  default = 10.5
 }
 
 
-variable "list"{
-  default =["naveen","rakshitha",10,"kruthika"]
+variable "list" {
+  default = ["naveen", "rakshitha", 10, "kruthika"]
 }
 
 
 variable "dictionary" {
-  default ={
-    "name"="reddy"
-    "age"=41
+  default = {
+    "name" = "reddy"
+    "age"  = 41
   }
 }
 
